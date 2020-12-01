fc99ba67b3b3d4170f0d0ecd57c29231d3c644eb
shajoffcl <shaj.native@gmail.com>
2020-12-01 23:42:48 +0530

    Rename feature added

---
+++ build/precache-manifest.292f3fa603691c9b70f9a9a8f545b4e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2d47b2339dc5b81160d342f62fea9e",
+    "url": "/movies-bucket%20/index.html"
+  },
+  {
+    "revision": "058cd9b2cef8197d0ef0",
+    "url": "/movies-bucket%20/static/css/main.ce8481f0.chunk.css"
+  },
+  {
+    "revision": "a6ee3eafe186192da6ba",
+    "url": "/movies-bucket%20/static/js/2.d57dcf28.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/movies-bucket%20/static/js/2.d57dcf28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "058cd9b2cef8197d0ef0",
+    "url": "/movies-bucket%20/static/js/main.fd94569d.chunk.js"
+  },
+  {
+    "revision": "5c801e7b81cfb7ab585d",
+    "url": "/movies-bucket%20/static/js/runtime-main.c5487d77.js"
+  }
+]);
